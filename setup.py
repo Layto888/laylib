8caf1b70cff876264c49a0a8c4ea3b1b14984827
Amine Amardjia <amardjia.amine@gmail.com>
2018-10-12 20:21:23 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="laylib",
-    version="1.0",
+    version="1.0.1",
 	license="MIT License",
     description="A 2-D game engine for Python",
     long_description=_long_description,

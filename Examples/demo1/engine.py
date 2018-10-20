696b6d9468d3fb9630c5b892a28705510a599b5b
Layto888 <amardjia.amine@gmail.com>
2018-10-20 18:59:55 +0100

    new example as demo to show how to automatically manage resources with laylib Demo1

--- Examples/demo1/engine.py
+++ Examples/demo1/engine.py
@@ -1,51 +1,56 @@
 import pygame as pg
-from random import randint
 from laylib import DefaultEngine
-from settings import (WIDTH, HEIGHT, NUM_CHAR_IMG, FPS)
-from body import Body, Background
+
+FPS = 30
+POS_Y = 22
+BLACK = (0, 0, 0)
 
 
 class Engine(DefaultEngine):
     """
-    Engine game example:
+    resources management example:
+
     this is the Main class of your game as example.
-    - Manage event, update and draw the whole demo.
+    - In this first example we show how to manage your resources:
+        1)- create a data folder and put all your resources into it.
+        2)- create a class named engine or whatever, this class represents your main
+        game engine.
+        3)- the DefaultEngine class will automatically load and separate resources types for you:
+            - self.img -> will contains the whole images
+            - self.snd -> will contains the whole fx sounds (wav, midi...)
+            - self.msc -> will contains the whole music (mp3, ogg)
+            - self.fnt -> will contains the whole fonts (ttf..)
+
+        4) - now we override the function draw to print the resources loaded as demo:
+            - see the function draw().
+
+        - ANY need to manage resources manually:
+          The thing is to put your resources in the data folder and they will be ready to use.
+
     """
 
     def __init__(self):
         super().__init__()
-        self.body = None
+        self.new_demo()
         self.all_sprites = pg.sprite.Group()
-        # setting new delta time, we're dividing pg.time.get_ticks() by 500.0
-        # the default value is 1000.0
-        self.time_unit = 500.0
-        print("time unit = {}".format(self.time_unit))
+        self.text_pos = POS_Y
 
     def update(self):
-        if not self.playing:
-            self.new_demo()
-        self.all_sprites.update(self.dt)
         self.clock.tick(FPS)
 
     def draw(self):
+        self.text_pos = POS_Y
 
-        self.all_sprites.draw(self.screen)
-        self.res.fnt.render(
-            self.fnt[0], 'Velocity body: {}'.format(self.body.vel), (10, 45)
-        )
-        self.res.fnt.render(
-            self.fnt[0], "FPS: {:.2f}".format(self.clock.get_fps()), (10, 20)
-        )
-        self.res.fnt.render(
-            self.fnt[0], "Dela time: {:.4f}".format(self.dt), (10, 70)
-        )
+        for res_file in self.res.pm.parser:
+            for data in self.res.pm.parser[res_file]:
+                if res_file != 'version':
+                    self.res.fnt.render(
+                        self.fnt[0], '{}: {} \n'.format(
+                            res_file, data), (1, self.text_pos)
+                    )
+                    self.text_pos += POS_Y
+
         pg.display.flip()
 
     def new_demo(self):
-        self.all_sprites.add(Background(self))
-
-        for _ in range(NUM_CHAR_IMG):
-            self.body = Body(self, (randint(0, WIDTH), HEIGHT))
-            self.all_sprites.add(self.body)
-
         self.playing = True
--- Examples/demo1/main.py
+++ Examples/demo1/main.py
@@ -3,22 +3,23 @@ Demo 01 Made with python and pygame
 to test and improve the laylib-pygame framework for fast game prototyping.
 ----
 Author: Amardjia Amine
-Date: 31/07/18
+Date: 20/10/18
 Github: ---
 
-TODO: -
+- This demo show how the Resources manager loads and
+  separates the different data and their associated variables.
 """
+
 from laylib import Environment
 from engine import Engine
-from settings import WIDTH, HEIGHT
 
 
 def main():
     # init global environment
     demo = Environment(
 
-        WIDTH,     # width
-        HEIGHT,    # height
+        800,       # width
+        600,       # height
         False,     # full screen
         '2D Demo'  # window title
     )

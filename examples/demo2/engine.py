c672acc5ea38491ae61df1a3d0da9bc522e9ef1c
Amine Amardjia <amardjia.amine@gmail.com>
2020-07-29 20:45:16 +0200

    last update demo 2 particles system

--- examples/demo2/engine.py
+++ examples/demo2/engine.py
@@ -8,20 +8,24 @@ from random import uniform
 """
 Parameters:
 1. The number of particles
-2. speed of particles on move
-3. the duration of each particle between creation and death
-4. the radius of each particle
-5. the field of generated particle
-6. the particles colors.
-7. background color
+2. Number of special particles (With longer lifetime)
+3. How many normal particles will be generated to generate one special particle
+4. speed of particles on move
+5. the duration of each particle between creation and death
+6. the radius of each particle
+7. the field of generated particle
+8. the particles colors.
+9. background color
 """
 
-PARTICLES_NUM = 1100
-X_MAX_SPEED = 335.0
-Y_MAX_SPEED = 530.0
-PARTICLE_DURATION = 380
-PARTICLE_RADIUS = 5
-THIKNESS = 2.0
+PARTICLES_NUM = 1500
+SPECIAL_PARTICLES_NUM = 100
+CYCLE_NRML_PARTICLES = 70
+X_MAX_SPEED = 300.0
+Y_MAX_SPEED = 290.0
+PARTICLE_DURATION = 67
+PARTICLE_RADIUS = 4
+THIKNESS = 0
 COLOR_RANGE = (80, 255)
 BLACK = (0, 0, 0)
 
@@ -36,6 +40,7 @@ class Engine(DefaultEngine):
     def __init__(self):
         super().__init__()
         self.new_demo()
+        self.cp = 0
 
     def update(self):
         # update all the group
@@ -44,9 +49,19 @@ class Engine(DefaultEngine):
                 # remove the dead particles
                 self.particles.remove(particle)
                 # here append new particle to replace the dead one
-                self.particles.append(
-                    self.generate_particle(pg.mouse.get_pos()))
+                if self.cp < CYCLE_NRML_PARTICLES:
 
+                    # Normal particles
+                    self.particles.append(
+                        self.new_particle(
+                            pg.mouse.get_pos(), uniform(PARTICLE_DURATION / 3.0, PARTICLE_DURATION * 3.0)))
+                    self.cp += 1
+                else:
+                    # specials particles : long lifetime duration
+                    self.particles.append(
+                        self.new_particle(
+                            pg.mouse.get_pos(), uniform(PARTICLE_DURATION * 9.0, PARTICLE_DURATION * 20.0)))
+                    self.cp = 0
             particle.update(self.dt)
 
     def draw(self):
@@ -58,19 +73,26 @@ class Engine(DefaultEngine):
     def new_demo(self):
         self.particles = []
         self.playing = True
+
         for _ in range(PARTICLES_NUM):
-            self.particles.append(self.generate_particle(pg.mouse.get_pos()))
+            self.particles.append(self.new_particle(
+                pg.mouse.get_pos(), uniform(PARTICLE_DURATION / 3.0, PARTICLE_DURATION * 3.0)))
+        for _ in range(SPECIAL_PARTICLES_NUM):
+            self.particles.append(self.new_particle(
+                pg.mouse.get_pos(), uniform(PARTICLE_DURATION * 9.0, PARTICLE_DURATION * 20.0)))
 
     @staticmethod
-    def generate_particle(old_pos):
-        pos = (old_pos[0],
-               old_pos[1])
+    def new_particle(old_pos, duration):
+        pos = (old_pos[0] + THIKNESS,  old_pos[1] + THIKNESS)
         vel = (uniform(-X_MAX_SPEED, X_MAX_SPEED),
                uniform(-Y_MAX_SPEED, Y_MAX_SPEED))
+
         radius = uniform(1, PARTICLE_RADIUS)
-        choice = randint(*COLOR_RANGE)
-        color = (randint(*COLOR_RANGE), randint(*COLOR_RANGE), choice)
-        return Particle(pos, vel, int(radius), color)
+        choice1 = old_pos[0] % 255
+        choice3 = old_pos[1] % 255
+        choice2 = randint(0, 255)
+        color = (choice1, choice3, choice2)
+        return Particle(pos, vel, int(radius), color, duration)
 
 
 class Particle(object):
@@ -78,12 +100,12 @@ class Particle(object):
     particle class
     """
 
-    def __init__(self, pos, vel, radius, color):
+    def __init__(self, pos, vel, radius, color, duration):
         self.x, self.y = pos
         self.vel = vect2d(vel)
         self.radius = radius
         self.color = color
-        self.duration = uniform(PARTICLE_DURATION / 3, PARTICLE_DURATION)
+        self.duration = duration
         self.startTime = pg.time.get_ticks()
 
     def update(self, dt):
